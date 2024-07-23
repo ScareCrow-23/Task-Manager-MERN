3808da0ca08fabfd5b2c8b28c2563ba2a31672bd
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-07-23 13:37:49 +0530

    Sidebar and Navbar Component completed

--- Documentation.md
+++ Documentation.md
@@ -1,14 +1,10 @@
 First Commit (22-07-2024) -
-    I have set up a Vite React app and started this project. Before getting started I installed a few packages I would be using in this project:
-        - Headless UI
-        - Redux Toolkit
-        - Clsx
-        - moment
-        - React Icons
-        - React Router Dom
-        - Recharts
-        -Sonner
-    After This I created a Folder Structure with the pages I will be using in this project.
+I have set up a Vite React app and started this project. Before getting started I installed a few packages I would be using in this project: - Headless UI - Redux Toolkit - Clsx - moment - React Icons - React Router Dom - Recharts
+-Sonner
+After This I created a Folder Structure with the pages I will be using in this project.
 
 Second Commit (22-07-2024) -
-    After Setting up the basics require for the app, I started making my first page. I added private and public routes in the app page, then started working on login page. The page consists of two containers, One with app name and motto and the other with a login form asking for email and password. I will be changing the name and motto when something creative comes to my mind but for now let it be what it is. I also created 2 components, a textbox component and a button component, both of these components will come in handy later in the project. 
+After Setting up the basics require for the app, I started making my first page. I added private and public routes in the app page, then started working on login page. The page consists of two containers, One with app name and motto and the other with a login form asking for email and password. I will be changing the name and motto when something creative comes to my mind but for now let it be what it is. I also created 2 components, a textbox component and a button component, both of these components will come in handy later in the project.
+
+Third Commit (23-07-2024) -
+I created some important components which can be used throught the application. First is a Sidebar component which has all the controlls a user and a admin has access to. Second is a Navbar component which has all the navigation buttons, I also made them responsive to work smothly on the mobile device as well on the desktop. Apart form these I also setup redux toolkit and its slices.
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,27 @@ import TaskDetails from "./pages/TaskDetails";
 import Tasks from "./pages/Tasks";
 import Trash from "./pages/Trash";
 import Users from "./pages/Users";
+import { useDispatch, useSelector } from "react-redux";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { setOpenSidebar } from "./redux/slices/authSlice";
+import { Transition } from "@headlessui/react";
+import { Fragment, useRef } from "react";
+import clsx from "clsx";
+import { IoClose } from "react-icons/io5";
 
 function Layout() {
-  const user = "";
+  const { user } = useSelector((state) => state.auth);
   const location = useLocation();
 
   return user ? (
     <div className="flex w-full h-screen flex-col md:flex-row">
       <div className="w-1/5 h-screen sticky bg-white top-0 hidden md:block">
-        {/* <Sidebar/> */}
+        <Sidebar />
       </div>
-      {/* <MobileSidebar /> */}
+      <MobileSidebar />
       <div className="flex-1 overflow-y-auto">
-        {/* <Navbar/> */}
+        <Navbar />
         <div className="p-4 2xl:px-10">
           <Outlet />
         </div>
@@ -29,6 +37,55 @@ function Layout() {
   );
 }
 
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMuneRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  return (
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter="transition-opacity duration-700"
+        enterFrom="opacity-x-10"
+        enterTo="opacity-x-100"
+        leave="transition-opacity duration-700"
+        leaveFrom="opacity-x-100"
+        leaveTo="opacity-x-0"
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={clsx(
+              "md:hidden w-full bg-black/40 transition-all duration-700 transform",
+              isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className="bg-white w-3/4 h-full ">
+              <div className="w-full flex justify-end px-5 mt-5">
+                <button
+                  onClick={() => closeSidebar()}
+                  className="flex justify-end iems-end"
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+              <div className="-mt-10">
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
